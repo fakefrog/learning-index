84f1cf37548c7840e83be1617920d79b6fbd668c
baiyundou <qwe6588038>
2019-12-05 13:54:59 +0800

    init

---
+++ dfire/src/main/java/com/wang/dfire/DfireApplication.java
@@ -0,0 +1,13 @@
+package com.wang.dfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfireApplication.class, args);
+    }
+
+}
